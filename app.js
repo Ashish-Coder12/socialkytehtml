fe5dce65341b1d6e45192c4777641aab3d2792e4
Ashish <yeshu.ya1716@gmail.com>
2021-05-25 13:36:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+function hide1(){
+    var acc1 = document.getElementById("first_heading")
+    var div1 = document.getElementById("first_pera")
+   if(div1.style.display!="none"){
+    div1.style.display="none"
+   }else{
+       div1.style.display="block"
+   }
+}
+function hide2(){
+    var acc2 = document.getElementById("second_heading")
+    var div2 = document.getElementById("second_pera")
+   if(div2.style.display!="none"){
+    div2.style.display="none"
+   }else{
+       div2.style.display="block"
+   }
+}
+function hide3(){
+    var acc3 = document.getElementById("third_heading")
+    var div3 = document.getElementById("third_pera")
+   if(div3.style.display!="none"){
+    div3.style.display="none"
+   }else{
+       div3.style.display="block"
+   }
+}
